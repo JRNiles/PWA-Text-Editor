124e1de91e1c5e541563429dc1e4b7b7d7daa398
Hogward <jaegern23@gmail.com>
2024-02-17 20:31:53 -0600

    add putDb and addDb methods

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,43 @@
 import { openDB } from 'idb';
 
-const initdb = async () =>
-  openDB('jate', 1, {
+const initdb = async () => {
+  const db = await openDB('jate', 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
+      if (!db.objectStoreNames.contains('jate')) {
+        const store = db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+        store.createIndex('content', 'content');
+        console.log('jate database created');
+      } else {
         console.log('jate database already exists');
-        return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
     },
   });
+  return db;
+};
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// Method to add content to the database
+export const putDb = async (content) => {
+  const db = await initdb();
+  const tx = db.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  await store.add({ content });
+  await tx.done;
+  console.log('Content added to database:', content);
+};
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// Method to get all content from the database
+export const getDb = async () => {
+  const db = await initdb();
+  const tx = db.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const content = await store.getAll();
+  await tx.done;
+  console.log('Content retrieved from database:', content);
+  return content;
+};
 
-initdb();
+initdb(); // Initialize database when the module is loaded
+
+// Usage examples:
+// putDb('Hello, World!');
+// getDb();
